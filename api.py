394a9ec082eaa374bd0668f3dfc51d8d8130c066
moyoonthego <39111972+moyoonthego@users.noreply.github.com>
2019-11-16 18:25:18 -0500

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ PLAY = "play"
 PAUSE = "pause"
 NEXT = "next"
 BACK = "back"
+SAD = "sad"
+HAPPY = "happy"
+MAD = "mad"
 
 from libsoundtouch import discover_devices
 from libsoundtouch import soundtouch_device
@@ -25,6 +28,23 @@ STdevice.pause()
 #STdevice.set_volume(device.volume() - 10)
 #device.play_media(Source.SPOTIFY, 'spotify:track:5J59VOgvclrhLDYUoH5OaW', 'moyoonthego')
 #device.play_url('https://www.youtube.com/watch?v=886A2ErYpQk')
+
+def button_response(input_val):
+    if (input_val == HAPPY):
+        device.play_media(Source.SPOTIFY, 'spotify:track:5J59VOgvclrhLDYUoH5OaW', 'moyoonthego')
+    elif (input_val == SAD):
+        device.play_url('https://www.youtube.com/watch?v=886A2ErYpQk')
+    elif (input_val == MAD):
+        device.play_url('https://www.youtube.com/watch?v=886A2ErYpQk')
+    elif (input_val == BACK):
+        STdevice.previous_track()
+    elif (input_val == NEXT):
+        STdevice.next_track()
+    elif (input_val == PLAY):
+        STdevice.play()
+    elif (input_val == PAUSE):
+        STdevice.pause()
+
 
 if __name__ == '__main__':
     pass
