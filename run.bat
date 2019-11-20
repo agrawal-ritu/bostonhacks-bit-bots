31335650ab203ee534e61a047c2a74ffe6719ebb
Ritu Agrawal <39224966+agrawal-ritu@users.noreply.github.com>
2019-11-20 10:26:04 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 SET BOSE_IP=192.168.1.14
-SET VISION_API_KEY=AIzaSyAAHZph9-nrKC-UzGi_08YChpG64pWokX4
+SET VISION_API_KEY=**
 SET GOOGLE_APPLICATION_CREDENTIALS=C:\Users\Moe\Documents\GitHub\bostonhacks-bit-bots\api_key.json
-SET SPOTIFY_API_KEY=31dlcuwpfbvet7ykhvmsdjytuhou
+SET SPOTIFY_API_KEY=**
 
 python mood_blasters.py
